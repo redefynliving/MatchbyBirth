d729fce116e0b31fe7daa2ec5aa0f194ebd81bfb
ajfox <redefynliving@gmail.com>
2026-06-26 16:36:07 -0400

    Save Sanity hosted studio app id

--- studio-matchbybirth/sanity.cli.ts
+++ studio-matchbybirth/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'iruq5mrfgjwhpgr6ki7ed7s1',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
